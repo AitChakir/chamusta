9706ef83a70e11f8e90c25492ecc56029d95b8a4
AitChakir <foldermusta@gmail.com>
2017-09-21 21:22:21 +0200

    agiuto devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919200422) do
+ActiveRecord::Schema.define(version: 20170921183524) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
